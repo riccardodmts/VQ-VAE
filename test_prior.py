dcb44c6a894dafa6ee43ef0a67f031d4af8d5f09
riccardodmts <riccardo@demonte.it>
2024-10-19 17:41:14 +0200

    ...

--- test_prior.py
+++ test_prior.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 from models.vqvae import VQVAE
 from models.pixelcnn import PixelCNN2d
 from torch.optim import Adam
